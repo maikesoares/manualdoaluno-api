4f5bc58c7b483e8f037e5e0f6fda1f57a5e45f21
Maike Soares <maikesoares2212@gmail.com>
2025-03-11 14:55:51 -0300

    Adicionado rota de busca do conteudo

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const port = 3000;
+const app = express();
+
+app.get("/content", (req, res) => {
+  res.send("Listagem dos Conteúdos");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor em execução na porta ${port}`);
+  
+})
