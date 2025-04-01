0de15fede1e05ac9731a8e20c35e347cab0ccf7f
Maike Soares <maikesoares2212@gmail.com>
2025-03-31 22:24:32 -0300

    Modificando BD

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,31 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
-app.get("/info", async (req, res) => {
+app.use(express.json());
+
+app.get("/info", async (_, res) => {
   const informacoes = await prisma.informacao.findMany();
   res.json(informacoes);
 });
+
+
+app.post("/info", async (req, res) => {
+
+  const {title , description } = req.body;
+
+  try {
+    await prisma.informacao.create({
+      data: {
+        title: title,
+        description: description,
+      }
+    });
+  }catch(error) {
+    console.log(error);
+    res.status(500).send({message: "Falha ao cadastar uma informação"});
+  }
+  res.status(201).send();
+})
 
 app.listen(port, () => {
   console.log(`Servidor em execução na porta ${port}`);
