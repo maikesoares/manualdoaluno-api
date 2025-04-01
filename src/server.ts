746386089bfc21d80566a725d5b870cb53aaab7f
Maike Soares <maikesoares2212@gmail.com>
2025-03-31 23:01:25 -0300

    Adicionado atualizacao do registro

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,15 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
+
+// ROTAS DA PAGINA DE INFORMACOES
+// Buscar Informação
 app.get("/info", async (_, res) => {
   const informacoes = await prisma.informacao.findMany();
   res.json(informacoes);
 });
 
-
+// Criar Informação
 app.post("/info", async (req, res) => {
 
   const {title , description } = req.body;
@@ -30,6 +33,37 @@ app.post("/info", async (req, res) => {
   }
   res.status(201).send();
 })
+
+// Atualizar Informação
+app.put("/info/:id", async (req, res) => {
+  const id = Number(req.params.id)
+
+  try{
+    const info = await prisma.informacao.findUnique({
+      where: {
+        id
+      }
+    });
+
+    if(!info){
+      return res.status(404).send({message: "Informação não encontrada."})
+    }
+
+    const data = {...req.body}
+    await prisma.informacao.update({
+      where: {
+        id: id
+      },
+      data: data
+    });
+  } catch (error){
+      console.log(error);
+      return res.status(500).send({message: "Falha ao atualizar o registro"})
+  }
+  res.status(200).send();
+})
+
+// Deletar Informação
 
 app.listen(port, () => {
   console.log(`Servidor em execução na porta ${port}`);
